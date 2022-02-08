a75d0f03436ee089c65ad6771039e5ac12c2048f
yuan hao Fan <fyuanhao91@gmail.com>
2022-02-09 03:12:57 +0800

    rename

---
+++ Csharp/ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    namespace qq
+    {
+        class Person
+        {
+            public string _name;
+            public int _age;
+            private double _height;
+            private double _width;
+            // static
+            public static int amount = 0;
+
+            // setter getter
+            public double Height
+            {
+                get { return _height; }
+                set { _height = value; }
+            }
+
+            public double Width
+            {
+                get { return _width; } 
+                set { _width = value; }
+            }
+
+            // construct
+            public Person(string name, int age, double h, double w)
+            {
+                _name = name;
+                _age = age;
+                Height = h;
+                Width = w;
+                amount++;
+            }
+
+
+            public void SayHi()
+            {
+                Console.WriteLine("hello i am " + _name);
+            }
+
+            public bool IsAdult()
+            {
+                return _age >= 18;
+            }
+
+            public int getAmount()
+            {
+                return amount;
+            }
+        }
+
+        // static class can't be create
+        static class Tool
+        {
+            // static method
+            public static void SayHi()
+            {
+                Console.WriteLine("Hello");
+            }
+        }
+
+        // inheritance
+        class Student : Person
+        {
+            public string _school;
+
+            public Student(string name, string school, int age, double h, double w):base(name, age, h, w)
+            {
+                _school = school;
+            }
+
+            public string getSchool()
+            {
+                return _school;
+            }
+
+        }
+
+    }
+
+}
---
+++ Csharp/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp1.qq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+        Person p1 = new Person("joe", 19, 163.0, 80); // new Person()
+
+        Console.WriteLine(p1._age);
+        p1.SayHi();
+        Console.WriteLine(p1.IsAdult());
+        p1 = new Person("Mary", 15, 152.0, 40);
+        p1.SayHi();
+        Console.WriteLine(p1.IsAdult());
+        Console.WriteLine(p1.Height);
+        Console.WriteLine(p1.Width);
+        Console.WriteLine(p1.getAmount());
+
+        Tool.SayHi();
+
+        Student s1 = new Student("hao", "singsisn", 18, 179.0, 70);
+
+        Console.WriteLine(p1.getAmount());
+        Console.WriteLine(s1.getSchool());
+    }
+}
+
+
